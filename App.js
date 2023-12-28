7e61b9e8c395cfcb82faa124c27f2421c6086324
MAC <mac@MACs-MacBook-Pro.local>
2023-12-28 16:00:43 +0100

    check for updates auto

--- App.js
+++ App.js
@@ -29,6 +29,8 @@ import PredictionScreen from './screens/PredictionScreen';
 import Toast, { BaseToast, ErrorToast } from 'react-native-toast-message';
 import Stroke from './screens/Stroke';
 import Diabetes from './screens/Diabetes';
+import { useEffect } from 'react';
+import * as Updates from 'expo-updates'
 
 const Stack = createStackNavigator();
 const toastConfig = {
@@ -70,6 +72,20 @@ const toastConfig = {
     }
 
 export default function App() {
+  async function onFetchUpdateAsync() {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    } catch (error) {
+      alert(`Error fetching latest Expo update: ${error}`);
+    }
+  }
+  useEffect(()=>{
+    onFetchUpdateAsync()
+  },[])
   return (
     <Provider store={store}>
     <NavigationContainer>
